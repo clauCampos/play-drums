1e5ef2e463b601c243e3ee2ad788e050842c3449
Claudia Campos <clau.acr@gmail.com>
2022-05-09 19:46:27 +0200

    css + changes-

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function delaySound(event) {
 }
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+/* // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -76,4 +76,4 @@ window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}); */
